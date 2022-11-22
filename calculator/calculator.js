6b06620ebf56e7ef39fd31eac77eb0c07d566620
ash730xo <109447309+ash730xo@users.noreply.github.com>
2022-11-21 22:27:46 -0500

    Add files via upload
    Calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,106 @@
+/* Getting Ids from HTML to add functionality  */
+/* Numbers IDs from HTML*/
+let zeroBttn = document.getElementById("zero")
+let oneBttn = document.getElementById("one")
+let twoBttn = document.getElementById("two")
+let threeBttn = document.getElementById("three")
+let fourBttn = document.getElementById("four")
+let fiveBttn = document.getElementById("five")
+let sixBttn = document.getElementById("six")
+let sevenBttn = document.getElementById("seven")
+let eightBttn = document.getElementById("eight")
+let nineBttn = document.getElementById("nine")
+
+/* Symbols Ids from html */
+let addBttn = document.getElementById("add")
+let subBttn = document.getElementById("sub")
+let multBttn = document.getElementById("mult")
+let divBttn = document.getElementById("divide")
+
+/* textbox ID from HTML */
+let textBttn = document.getElementById("textbox")
+
+/* Clear Button */
+let clearBttn = document.getElementById("clear")
+
+/* Submit Button */
+let submitBttn = document.getElementById("submit")
+
+/* number button on click events  */
+zeroBttn.onclick = function() {
+
+    alert("0")
+}
+
+oneBttn.onclick = function() {
+
+    alert("1")
+}
+
+twoBttn.onclick = function() {
+
+    alert("2")
+}
+
+threeBttn.onclick = function() {
+
+    alert("3")
+}
+
+fourBttn.onclick = function() {
+
+    alert("4")
+}
+
+fiveBttn.onclick = function() {
+
+    alert("5")
+}
+
+sixBttn.onclick = function() {
+
+    alert("6")
+}
+
+sevenBttn.onclick = function() {
+
+    alert("7")
+}
+
+eightBttn.onclick = function() {
+
+    alert("8")
+}
+
+nineBttn.onclick = function() {
+
+    alert("9")
+}
+
+/* symbols button on click events  */
+addBttn.onclick = function() {
+
+    alert("+")
+}
+subBttn.onclick = function() {
+
+    alert("-")
+}
+multBttn.onclick = function() {
+
+    alert("*")
+}
+divBttn.onclick = function() {
+
+    alert("/")
+}
+
+clearBttn.onclick = function(){
+    alert("Clear")
+}
+
+submitBttn.onclick = function(){
+    alert("Submit Bitchesssss")
+}
+
+
